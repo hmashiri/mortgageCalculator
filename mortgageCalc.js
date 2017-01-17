8c4044768b8a7860762e53f2d4414159571ab61e
hmashiri <hadjirmashiri@gmail.com>
2017-01-17 14:05:59 -0800

    cleaned up mortgage code

--- mortgageCalc.js
+++ mortgageCalc.js
@@ -4,33 +4,29 @@ var paymentType =
 	[ "bi-monthly", 6 ], 
 ];
 
-    paymentType.forEach( function(valueIndex)
-    {
-      $('#paymentPeriod').append($("<option/>").text(valueIndex[0]).val(valueIndex[1]));
-    });
+paymentType.forEach( function(valueIndex)
+{
+        $('#paymentPeriod').append($("<option/>").text(valueIndex[0]).val(valueIndex[1]));
+});
 
 
 $(document).ready(function()
 {
 	$('#onclick').click( function()
+    	{
+		var interestRate = $("#interestRateInput").val();
+		var loanBalance = $('#loanBalanceInput').val();
+		var loanTerm = $('#termInYearsInput').val();
+		var periodIndex = $('#paymentPeriod option:selected').text();        
+		var period = $('#paymentPeriod option:selected').val();
 
+		var monthlyInterestRate = (interestRate / 100) / period;          
+			var numberOfPayments = loanTerm * period;
+			var compoundedInterestRate = Math.pow( (1 + monthlyInterestRate), numberOfPayments );  
+			var interestQuotient  = (monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1);
+			var monthlyPayment = loanBalance * interestQuotient;
 
-    {
-    	var interestRate = $("#interestRateInput").val();
-        var loanBalance = $('#loanBalanceInput').val();
-        var loanTerm = $('#termInYearsInput').val();
-        var periodIndex = $('#paymentPeriod option:selected').text();        
-        var period = $('#paymentPeriod option:selected').val();
-
-        var monthlyInterestRate = (interestRate / 100) / period;          
-  		var numberOfPayments = loanTerm * period;
-  		var compoundedInterestRate = Math.pow( (1 + monthlyInterestRate), numberOfPayments );  
- 		var interestQuotient  = (monthlyInterestRate * compoundedInterestRate) / (compoundedInterestRate - 1);
-  		var monthlyPayment = loanBalance * interestQuotient;
-
-        $('#results').text("Your " + periodIndex + " payment is $" + monthlyPayment.toFixed(2) + ". Thank you!");
-    });
-
-
+		$('#results').text("Your " + periodIndex + " payment is $" + monthlyPayment.toFixed(2) + ". Thank you!");
+    	});
 
 });
